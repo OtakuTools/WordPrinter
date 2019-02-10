3658b056f851436b5d557fc7e79dea82dc3ae498
ZTao-z <1124693098@qq.com>
2019-02-10 12:10:49 +0800

    update graph drawing

--- Word-Printer/generateGraph.py
+++ Word-Printer/generateGraph.py
@@ -2,21 +2,66 @@
 from graphviz import Digraph
 
 class drawGraph:
+
+    styles = {
+        #'graph': {
+        #    'label': 'A Fancy Graph',
+        #    'fontsize': '16',
+        #    'fontcolor': 'white',
+        #    'bgcolor': '#333333',
+        #    'rankdir': 'BT',
+        #},
+        'nodes': {
+            'fontname': 'KaiTi',
+            'shape': 'box',
+            'fontcolor': 'white',
+            'color': 'white',
+            'style': 'filled',
+            'fillcolor': '#006699',
+        },
+        #'edges': {
+        #    'style': 'dashed',
+        #    'color': 'white',
+        #    'arrowhead': 'open',
+        #    'fontname': 'Courier',
+        #    'fontsize': '12',
+        #    'fontcolor': 'white',
+        #}
+    }
+
     def __init__(self):
         pass
 
     def __del__(self):
         pass
 
+    def preview(self, graph):
+        graph.view()
+
+    def save(self, graph):
+        graph.render(filename="./result.gv", view=True)
+
+    def apply_styles(self, graph):
+        graph.graph_attr.update(
+            ('graph' in self.styles and self.styles['graph']) or {}
+        )
+        graph.node_attr.update(
+            ('nodes' in self.styles and self.styles['nodes']) or {}
+        )
+        graph.edge_attr.update(
+            ('edges' in self.styles and self.styles['edges']) or {}
+        )
+        return graph
+
     def draw(self, data):
         pass
 
     def testDraw(self):
-        dot = Digraph(comment="Thre round Table")
-        dot.node('A', 'King')
+        dot = Digraph(comment="Thre round Table", format="png")
+        dot.node('A', "中文")
         dot.node('B', 'Knight')
         dot.node('C', 'Solider')
         dot.edges(['AB','AC'])
-
+        dot = self.apply_styles(dot)
         print(dot.source)
-        dot.render(filename="./result.gv", format="png")
+        self.preview(dot)
