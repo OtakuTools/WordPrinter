1ee2e44451927054e3d25a62bc0423cb1516d983
Ming-zz <184653774@qq.com>
2019-02-10 08:31:38 +0800

    添加了graphviz库

---
+++ Word-Printer/generateGraph.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import markdown
+from graphviz import Digraph
+
+class drawGraph:
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
+
+    def testDraw(self):
+        dot = Digraph(comment="Thre round Table")
+        dot.node('A', 'King')
+        dot.node('B', 'Knight')
+        dot.node('C', 'Solider')
+        dot.edges(['AB','AC'])
+
+        print(dot.source)
+        dot.render(filename="./result.gv", format="png")
--- Word-Printer/main.py
+++ Word-Printer/main.py
@@ -4,9 +4,13 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from test import Ui_MainWindow
 from database import DB
+from generateGraph import drawGraph
 
 if __name__ == '__main__':
     db = DB()
+    graph = drawGraph()
+
+    graph.testDraw()
 
     app = QApplication(sys.argv)
     MainWindow = QMainWindow()
