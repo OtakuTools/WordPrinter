9ec06b8a98ea87aec69b14bc5e9d4bc9a891d17d
Ming-zz <184653774@qq.com>
2019-02-11 18:00:47 +0800

    修改了插入图片的尺寸

--- Word-Printer/pictureIns.py
+++ Word-Printer/pictureIns.py
@@ -3,6 +3,7 @@ from docx.shared import Inches
 from docx.shared import RGBColor
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.enum.text import WD_COLOR_INDEX
+from docx.shared import Cm
 
 def pictureIns(src,dst,pic):
     document = Document(src)
@@ -12,7 +13,7 @@ def pictureIns(src,dst,pic):
                 r.font.highlight_color = None
                 r.font.color.rgb = RGBColor(0x00, 0x00, 0x00)
                 pp = p.insert_paragraph_before()
-                pp.add_run().add_picture(pic)
+                pp.add_run().add_picture(pic,Cm(16))
                 pp.alignment = WD_ALIGN_PARAGRAPH.CENTER
     document.save(dst)
     print('图片插入成功')
