1965ae6cc266d771b4693ebea178852593d67fb9
Ming-zz <184653774@qq.com>
2019-02-19 00:48:22 +0800

    UI实现了删除部门，但最后一个删不掉（已定位，删掉最后一个时会触发currentChanged）

--- Word-Printer/main.py
+++ Word-Printer/main.py
@@ -12,6 +12,7 @@ from dataStruct import userInfo
 from UIfunc import Controller
 
 if __name__ == '__main__':
+    '''
     db = DB()
 
     user = userInfo();
@@ -26,8 +27,8 @@ if __name__ == '__main__':
     print(db.update("info", {"company" : "广州真如信息科技有限公司", "address" : "b", "id" : "asdasdasd"}))
     #print(db.update("department", {"name" : "客户服务中心", "intro":["hello", "world"], "func" : [3,4], "refId" : "广州真如信息科技有限公司"}))
     #print(db.delete("department", "ABMM", "aaa") )
-
-    #app = QApplication(sys.argv)
-    #window = Controller()
-    #window.show()
-    #sys.exit(app.exec_())
+    '''
+    app = QApplication(sys.argv)
+    window = Controller()
+    window.show()
+    sys.exit(app.exec_())
